17593c9afa18b6540d03545f1635d56900012e16
Luisa Silva <luisaclara04@gmail.com>
2026-09-16 23:50:00 -0300

    Resolve K01 IA, K04 IA, K06 IA #36 #39 #41

---
+++ lab02/code/trials/Luisa/IA/K04/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] gardenNoAdjacent(int n, int[][] paths) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) graph.add(new ArrayList<>());
+
+        for (int[] p : paths) {
+            graph.get(p[0]).add(p[1]);
+            graph.get(p[1]).add(p[0]);
+        }
+
+        int[] result = new int[n + 1];
+
+        for (int garden = 1; garden <= n; garden++) {
+            boolean[] usedColors = new boolean[5];
+            for (int neighbor : graph.get(garden)) {
+                if (result[neighbor] != 0) {
+                    usedColors[result[neighbor]] = true;
+                }
+            }
+            for (int color = 1; color <= 4; color++) {
+                if (!usedColors[color]) {
+                    result[garden] = color;
+                    break;
+                }
+            }
+        }
+
+        return Arrays.copyOfRange(result, 1, n + 1);
+    }
+}
