0086ed38ce4d58d736a013500021e015e55afe14
Matheus Gaston <theteusviana@gmail.com>
2026-09-16 21:20:13 -0300

    code: resolve k03 closes #26

---
+++ lab02/code/trials/matheus_gaston/IA/K03/Solution.java
@@ -0,0 +1,25 @@
+package katas.k03;
+
+/**
+ * K03 - House Robber (issue #22).
+ * https://leetcode.com/problems/house-robber/
+ *
+ * Dado um vetor nums em que nums[i] e o valor guardado na i-esima casa de
+ * uma rua, determina o valor maximo que pode ser roubado sem roubar duas
+ * casas adjacentes.
+ */
+public class Solution {
+
+    public int rob(int[] nums) {
+        int previous = 0;
+        int current = 0;
+
+        for (int num : nums) {
+            int bestWithCurrentHouse = previous + num;
+            previous = current;
+            current = Math.max(current, bestWithCurrentHouse);
+        }
+
+        return current;
+    }
+}
